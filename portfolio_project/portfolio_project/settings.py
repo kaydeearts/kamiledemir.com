3a55f298f328a06a1ff016402e41dd1f1ad45bbf
Kamile <kamile.kaydee@gmail.com>
2020-06-30 02:29:21 -0400

    added allowed host for python anywhere

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#f-%9@lw5%=6oaf&g^3mh7p)2^^(081)bf4$vx733s$ja+!m1m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kamiledemir.pythonanywhere.com']
 
 
 # Application definition
